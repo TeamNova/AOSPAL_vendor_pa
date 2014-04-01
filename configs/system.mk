e3badc550f63169c8ba16b96683924b3db672cea
jescowalker <jescowalker@gmail.com>
2014-04-01 03:30:11 -0700

    SuperUser revert [2/3]

--- configs/system.mk
+++ configs/system.mk
@@ -34,8 +34,6 @@ PRODUCT_COPY_FILES += \
     vendor/$(VENDOR)/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/$(VENDOR)/prebuilt/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
 
-# SU Support
-SUPERUSER_EMBEDDED := true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
@@ -44,5 +42,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
     org.cyanogenmod.hardware.xml
+
+# SU Support
+PRODUCT_COPY_FILES += \
+    vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/daemonsu \
+    vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/su \
+    vendor/$(VENDOR)/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/$(VENDOR)/prebuilt/apk/Superuser.apk:system/priv-app/Superuser.apk
+
 
 $(call inherit-product, vendor/psd/configs/psd_board.mk)
