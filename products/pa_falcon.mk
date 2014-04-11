fc54e62218c12246aa8fa4382125f7df3d6975d8
TheMrcool212 <themrcool212.android@gmail.com>
2014-04-11 20:03:11 +0800

    fix conflicts

--- products/pa_falcon.mk
+++ products/pa_falcon.mk
@@ -33,15 +33,7 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Override AOSP build properties
 PRODUCT_NAME := pa_falcon
-<<<<<<< HEAD
-<<<<<<< HEAD
-PRODUCT_DEVICE := full_falcon
-=======
 PRODUCT_DEVICE := falcon
->>>>>>> PA/kitkat
-=======
-PRODUCT_DEVICE := falcon
->>>>>>> PA/kitkat
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := falcon
